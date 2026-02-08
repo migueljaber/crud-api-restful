1f2aa84f06196705ffba413312305b3680df77bb
migueljaber <migueljabermacedo@gmail.com>
2026-02-08 14:23:27 -0300

    chore: bootstrap Spring Boot project

---
+++ src/main/java/com/senai/migueljaber/crudapirestful/CrudApiResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.senai.migueljaber.crudapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/migueljaber/crudapirestful/CrudApiResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.migueljaber.crudapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
